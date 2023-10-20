3343dc3e4d3a54e5260050763e9ba95d85a094a5
yolanda-y-li <yolandayli1600@gmail.com>
2023-10-20 18:45:19 -0400

    Finish buttons and format train component

--- exer5/marta-interface/src/components/Train.js
+++ exer5/marta-interface/src/components/Train.js
@@ -1,7 +1,27 @@
-export default function Train() {
+import rightArrow from "../images/rightArrow.png";
+
+export default function Train(props) {
+    const { trainData } = props;
+    console.log(rightArrow);
+
     return (
         <div className="train-component">
-            Train
+            <div className="train-left-info">
+                <div className="train-path">
+                    <p className="train-path-text">Midtown</p>
+                    <img src="" className="right-arrow" />
+                    <p className="train-path-text">Airport</p>
+                </div>
+                <div className="train-status">
+                    <div className="train-line">Gold</div>
+                    <p className="on-time">On time</p>
+                </div>
+            </div>
+            <p className="train-time">
+                    3
+                    <br />
+                    min
+            </p>
         </div>
     )
 }
--- exer5/marta-interface/src/components/TrainList.js
+++ exer5/marta-interface/src/components/TrainList.js
@@ -1,9 +1,37 @@
+import Train from "./Train";
+import { useState } from "react";
+
 export default function TrainList(props) {
+    const [tab, setTab] = useState("Arriving");
     const { currColor, trainData } = props;
+    const arrivals = trainData["RailArrivals"];
+
+    function setBtnStyle(thisTab) {
+        return (
+            {
+                backgroundColor: tab === thisTab ? "rgb(41, 41, 41)" : "white",
+                border: tab === thisTab ? "2px solid rgb(41, 41, 41)" : "2px solid #b6b6b6",
+                color: tab === thisTab ? "white": "black"
+            }
+        )
+    }
 
     return (
         <div className="trainlist-component">
-            TrainList
+            <div className="buttons-container">
+                <button className="button" style={setBtnStyle("Arriving")}>Arriving</button>
+                <button className="button" style={setBtnStyle("Scheduled")}>Scheduled</button>
+                {currColor === "GOLD" | currColor == "RED" ?
+                    <><button className="button" style={setBtnStyle("Northbound")}>Northbound</button>
+                    <button className="button" style={setBtnStyle("Southbound")}>Southbound</button></>
+                    :
+                    <><button className="button" style={setBtnStyle("Eastbound")}>Eastbound</button>
+                    <button className="button" style={setBtnStyle("Westbound")}>Westbound</button></>
+                }
+            </div>
+            <div className="trains-container">
+                <Train />
+            </div>
         </div>
     )
 }
